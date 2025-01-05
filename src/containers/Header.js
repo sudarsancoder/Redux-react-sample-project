455f30d5502827f93106c9d8bfba60b88c75ff82
SUDARSAN <118043122+sudarsancoder@users.noreply.github.com>
2025-01-05 18:51:36 +0530

    Update Header.js a tag

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div className="ui fixed menu">
       <div className="ui container center">
-        <h2><a href="https://example.com" target="_blank">FakeShop</a></h2>
+        <h2><a href="https://redux-shoppingsite.netlify.app/" target="_blank">FakeShop</a></h2>
       </div>
     </div>
   );
